88e359e026e973922ec2a4ffe20aed1fb2095ca6
hujiacheng <hujiacheng@iipcloud.onaliyun.com>
2025-11-07 14:39:01 +0800

    fix: Correct ESLint configuration syntax

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -43,4 +43,4 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
-};
+}
